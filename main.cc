321294742b2259e8d733fbb70e55137fba7c3c45
Milton <mperaza1@binghamton.edu>
2020-05-04 18:00:38 -0400

    Revert "fix seg by changing alloc order"
    This reverts commit 2f5a9b7a2e56d3ed800e9ff28f263c1a4b86809c.

--- main.cc
+++ main.cc
@@ -4,10 +4,9 @@
 
 int main(int argc, char *argv[])
 {
-	ProfileInit();
 	Stamp start;
-	std::cout << "Start stamp" << std::endl;
 
+	ProfileInit();
 	//auto e = getEnergySamples(100000,2);
 	//e->printArrays();
 	void (*fns[])(double[],int ) = {bubbleSort, insertionSort, mergeSort, mergeSortOpt, quickSort, quickSort_optimized};
@@ -17,20 +16,15 @@ int main(int argc, char *argv[])
 		std::cout << name << std::endl;
 	}
 	
-	
-	this_thread::sleep_for(chrono::milliseconds(1));
+	ProfileDealloc();
+
 	Stamp stop;
-	std::cout << "stop stamp" << std::endl;
 
 	auto time_elapsed = stop.timeDifference(start);
-	std::cout << "stop timeDifference" << std::endl; 
-
 	std::vector<double> energy_diff = stop.energyDifference(start);
 
 	std::cout << "time " << time_elapsed << std::endl;
 	printVector(energy_diff);
-	std::cout << "print vector" << std::endl;
 	
-	ProfileDealloc();
 	return 0;
 }
