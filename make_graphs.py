edb50231392c504720535523aa8919ba9cc98403
Rutvik <rutvik20007@gmail.com>
2020-05-04 13:44:19 -0400

    Completed graph script

--- make_graphs.py
+++ make_graphs.py
@@ -13,15 +13,29 @@ for file in listdir():
                 words = line.split()
                 tup = (int(words[1]), int(words[2]), float(words[3]))
                 info[words[0]].append(tup)
+
 energy_plot = plt.figure()
-time_plot = plt.
+ax1 = energy_plot.add_subplot(111)
+time_plot = plt.figure()
+ax2 = time_plot.add_subplot(111)
 
 for key in info.keys():
     lengths = []
     energies = []
     times = []
     for value in info[key]:
-        lengths.append(value[1])
-        times.append(value[2])
-        energies.append(value[3])
-    plt.plot()
+        lengths.append(value[0])
+        times.append(value[1])
+        energies.append(value[2])
+    print(lengths, energies, times, key)
+    ax1.plot(lengths, times, label = key)
+    ax2.plot(lengths, energies, label = key)
+
+ax1.set_xlabel("Array length (n)")
+ax1.set_ylabel("Energy (J)")
+ax1.legend()
+ax2.set_xlabel("Array length (n)")
+ax2.set_ylabel("Energy (J)")
+ax2.legend()
+energy_plot.savefig("energy_plot")
+time_plot.savefig("time_plot")
