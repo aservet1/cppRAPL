7cf43afb8459dc8972a5899fe3d040beaa693e66
Gitea <aservet1@binghamton.edu>
2020-05-04 23:05:13 -0400

    fixed bogosort to compile when added to main.cc

--- algorithms/bogoSort.hh
+++ algorithms/bogoSort.hh
@@ -3,28 +3,12 @@
 
 #include <iostream>
 #include <ctime>
-
-template <typename E>
-static bool isSorted(E arr[], int length){
-  for (int i = 1; i < length; i++){
-    if (arr[i] < arr[i-1]){
-      return false;
-    }
-  }
-  return true;
-}
-
-template <typename E>
-static void swap(E arr[], int i, int j){
-  E temp = arr[i];
-  arr[i] = arr[j];
-  arr[j] = temp;
-}
+#include "sorts.hh"
 
 template <typename E>
 static void shuffle(E arr[], int length){
   for (int n = 0; n < length*2; n++){
-    int i = rand()%length
+    int i = rand()%length;
     int j = rand()%length;
     swap(arr, i, j);
   }
