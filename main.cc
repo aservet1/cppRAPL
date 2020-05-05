800ee369814f19b82d56da644787d95a804db07e
Gitea <aservet1@binghamton.edu>
2020-05-05 03:07:50 -0400

    bogosort works now

--- main.cc
+++ main.cc
@@ -15,42 +15,51 @@
 
 #define DELAY 2
 
-
-int main(int argc, char *argv[]){
-  ProfileInit();
-
-  void (*fns[])(double[],int ) = {/*bogoSort,*/ bubbleSort, insertionSort, /*countingSort,*/ mergeSort, mergeSortOpt,/* quickSort, quickSortOpt,*/ heapSort};
-
-  const char *fnNames[] = {/*"Bogo_Sort",*/ "Bubble_Sort", "Insertion_Sort" /*"Counting_Sort"*/, "Merge_Sort", "Optimized_Mergesort", /*"Quicksort", "Optimized_Quicksort"*/ "Heap_Sort"};
-	
-  //std::cout << "len of names " << sizeof(fnNames)/sizeof(char*) << std::endl;
-
-  int sizes[] = {10, 20, 30, 40 ,50 , 60, 70, 80, 90, 100,200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};//, 10000, 50000, 1000000};
-  for(int size : sizes){
+void doTheThing(int numberOfSorts, void (*fns[])(double[],int), std::string fnNames[], std::vector<int> sizes)
+{ //@TODO make a new name
+  for(int size : sizes)
+  {
     //EnergyArrays *arrays = getEnergySamples(size, DELAY);
     double *data = new double[size];
     genRandomArray(data, size);
     double *copy = new double[size];
-    for(int i = 0; i < 5; i++){
-      if((i == 0 || i == 1) && size > 50000){
+    for(int i = 0; i < numberOfSorts; i++)
+    {
+      if((fnNames[i] == "Bubble_Sort" || fnNames[i] == "Insertion_Sort" || fnNames[i] == "Selection_Sort") && size > 50000)
+      { //@TODO is the name in the list?
         continue;
       }
       makeCopy(copy, data, size);
       Stamp start;
       fns[i](copy, size);
       Stamp stop;
-      double time_elapsed = stop.timeDifference(start) / 1000000;
+      double time_elapsed;
+      if (fnNames[i] == "BOGO_Sort") time_elapsed = stop.timeDifference(start);
+      else time_elapsed = stop.timeDifference(start) / 1000000;
       std::vector<double> energy_diff = stop.energyDifference(start);
-      std::cout << fnNames[i] << " " << size << " "  << time_elapsed << " " ;
+      std::cout << fnNames[i] << " " << size << std::setprecision(20) << " "  << time_elapsed << " " ;
       printVector(energy_diff);
     }
     delete[] copy;
     delete[] data;
-    //delete arrays;
-  }
-  for(int i = 0; i < 20; i++){
-	  
-  }
+    //delete arrays 
+  } 
+}
+
+int main(int argc, char *argv[]){
+  ProfileInit();
+
+  int numberOfSorts = 6;
+  void (*fns[])(double[],int ) = {bubbleSort, insertionSort, countingSort, mergeSort, mergeSortOpt, heapSort};
+  std::string fnNames[] = {"Bubble_Sort", "Insertion_Sort", "Counting_Sort", "Merge_Sort", "Optimized_Mergesort", "Heap_Sort"};
+  std::vector<int> sizes = {10, 20, 30, 40 ,50 , 60, 70, 80, 90, 100,200, 300, 400, 500, 600, 700, 800, 900, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000, 10000, 20000, 30000, 40000, 50000, 60000, 70000, 80000, 90000, 100000};
+
+  doTheThing(numberOfSorts, fns, fnNames, sizes);
+
+  void (*bogo[])(double[],int) = {bogoSort};
+  std::string bogoName[] = {"BOGO_Sort"};
+  std::vector<int> bogoSizes = {1,2,3,4,5,6,7,8,9,10,11,12};
+  doTheThing(1, bogo, bogoName, bogoSizes);
 
   ProfileDealloc();
 
